2cf8424fc596e71cb6e4bff7ad156afe6d01a4ab
Michał Sajnóg <msajnog93@gmail.com>
2018-11-23 17:20:52 +0100

    Clean code, add spinner, improve messages in terminal

--- s3deploy.js
+++ s3deploy.js
@@ -1,4 +1,4 @@
-const { info, error, logWithSpinner, stopSpinner } = require('@vue/cli-shared-utils')
+const { info, error } = require('@vue/cli-shared-utils')
 const path = require('path')
 const fs = require('fs-extra')
 const mime = require('mime-types')
@@ -6,8 +6,11 @@ const globby = require('globby')
 const AWS = require('aws-sdk')
 const PromisePool = require('es6-promise-pool')
 const zlib = require('zlib');
+const ora = require('ora');
 
 const deployDir = 'dist-deploy';
+
+const spinner = ora();
 
 let S3;
 
@@ -17,8 +20,35 @@ function mimeCharsetsLookup(mimeType, fallback) {
   return (/^text\/|^application\/(javascript|json)/).test(mimeType) ? 'UTF-8' : fallback;
 }
 
-function contentTypeFor (filename) {
+function contentTypeFor(filename) {
   return mime.lookup(filename) || 'application/octet-stream'
+}
+
+async function setupAWS(awsProfile, region) {
+  const awsConfig = {
+    region: region,
+    httpOptions: {
+      connectTimeout: 30 * 1000,
+      timeout: 120 * 1000
+    }
+  }
+
+  if (awsProfile.toString() !== 'default') {
+    const credentials = new AWS.SharedIniFileCredentials({
+      profile: awsProfile
+    })
+
+    await credentials.get((err) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      awsConfig.credentials = credentials
+    })
+  }
+
+  AWS.config.update(awsConfig)
+  return new AWS.S3()
 }
 
 async function createBucket (options) {
@@ -60,7 +90,7 @@ async function enableStaticHosting (options) {
   // enable static hosting
   try {
     await S3.putBucketWebsite(staticParams).promise()
-    info(`Static Hosting is enabled.`)
+    info('Static Hosting is enabled.')
   } catch (staticErr) {
     error(`Static Hosting could not be enabled on bucket: ${options.bucket}. AWS Error: ${staticErr.toString()}.`)
   }
@@ -104,6 +134,16 @@ function getAllFiles (assetPath, pattern = '**') {
     }));
 }
 
+function parseDeployPath (depPath) {
+  let deployPath = depPath
+  // We don't need a leading slash for root deploys on S3.
+  if (deployPath.startsWith('/')) deployPath = deployPath.slice(1, deployPath.length)
+  // But we do need to make sure there's a trailing one on the path.
+  if (!deployPath.endsWith('/') && deployPath.length > 0) deployPath = deployPath + '/'
+
+  return deployPath
+}
+
 async function invalidateDistribution (options) {
   const cloudfront = new AWS.CloudFront()
   const invalidationItems = options.cloudfrontMatchers.split(',')
@@ -119,23 +159,23 @@ async function invalidateDistribution (options) {
     }
   }
 
-  logWithSpinner(`Invalidating CloudFront distribution: ${options.cloudfrontId}`)
+  spinner.start(`Invalidating CloudFront distribution: ${options.cloudfrontId}`)
 
   try {
     let data = await cloudfront.createInvalidation(params).promise()
+
+    spinner.succeed();
 
     info(`Invalidation ID: ${data['Invalidation']['Id']}`)
     info(`Status: ${data['Invalidation']['Status']}`)
     info(`Call Reference: ${data['Invalidation']['InvalidationBatch']['CallerReference']}`)
     info(`See your AWS console for on-going status on this invalidation.`)
   } catch (err) {
-    error('Cloudfront Error!')
+    spinner.fail('Invalidating CloudFront distrubution failed.');
     error(`Code: ${err.code}`)
     error(`Message: ${err.message}`)
     error(`AWS Request ID: ${err.requestId}`)
   }
-
-  stopSpinner()
 }
 
 async function uploadFile ({ fileKey, fileBody, options, gzip }) {
@@ -167,10 +207,10 @@ async function uploadFile ({ fileKey, fileBody, options, gzip }) {
     uploadParams.CacheControl = 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
   }
 
-  console.log('upload', { uploadParams });
-
   try {
+    spinner.start(`Uploading: ${uploadParams.Key}`);
     await S3.upload(uploadParams, options.uploadOptions).promise()
+    spinner.clear();
   } catch (uploadResultErr) {
     // pass full error with details back to promisePool callback
     throw new Error(`(${options.uploadCount}/${options.uploadTotal}) Upload failed: ${fileKey}. AWS Error: ${uploadResultErr.toString()}.`)
@@ -196,114 +236,90 @@ async function prepareDeploymentDirectory (assetPath, assetMatch) {
   }, 10);
 
   try {
-    info('Preparing deployment directory...');
-
-    await fs.emptyDir(getFullPath(deployDir));
-    await copyPool.start();
-
-    info('Deployment directory created.');
+    spinner.start('Creating deployment directory')
+    await fs.emptyDir(getFullPath(deployDir))
+    await copyPool.start()
+    spinner.succeed('Deployment directory created.')
   } catch (err) {
-    error('Something went wrong...')
+    spinner.fail('Deployment directory could not be created.')
+    error(err)
+  }
+}
+
+async function gzipFiles(filesToGzip) {
+  const filesQueue = [...filesToGzip]
+
+  const gzipPool = new PromisePool(() => {
+    if (filesQueue.length === 0) return null
+
+    const filePath = filesQueue.pop()
+    const outputPath = `${filePath}.gz`
+    const gzip = zlib.createGzip()
+
+    return new Promise((resolve, reject) => {
+      const input = fs.createReadStream(filePath)
+      const output = fs.createWriteStream(outputPath)
+
+      input.pipe(gzip).pipe(output)
+
+      input.on('error', (err) => {
+        reject(err)
+      });
+
+      output.on('error', (err) => {
+        reject(err)
+      });
+
+      output.on('finish', () => {
+        resolve()
+      });
+    }).then(() => fs.rename(outputPath, filePath))
+  }, 10)
+
+  try {
+    spinner.start('Gzipping files')
+    await gzipPool.start()
+    spinner.succeed(`All ${filesToGzip.length} have been gzipped successfully.`)
+  } catch (err) {
+    spinner.fail('Files haven\'t been gzipped properly.')
     error(err)
   }
 }
 
 module.exports = async (options, api) => {
-  info(`Options: ${JSON.stringify(options)}`)
-
-  let awsConfig = {
-    region: options.region,
-    httpOptions: {
-      connectTimeout: 30 * 1000,
-      timeout: 120 * 1000
-    }
+  try {
+    spinner.start('Setting up AWS')
+    S3 = await setupAWS(options.awsProfile, options.region)
+    spinner.succeed('AWS credentials confirmed')
+  } catch (err) {
+    spinner.fail('Setting up AWS failed.')
+    return error(err)
   }
 
-  if (options.awsProfile.toString() !== 'default') {
-    let credentials = new AWS.SharedIniFileCredentials({ profile: options.awsProfile })
-    await credentials.get((err) => {
-      if (err) {
-        error(err.toString())
-      }
-
-      awsConfig.credentials = credentials
-    })
-  }
-
-  AWS.config.update(awsConfig)
-  S3 = new AWS.S3()
-
-  if (await bucketExists(options) === false) {
-    error('Deployment terminated.')
-    return
-  }
+  if (await bucketExists(options) === false) return
 
   options.uploadOptions = { partSize: (5 * 1024 * 1024), queueSize: 4 }
 
-  await prepareDeploymentDirectory(options.assetPath, options.assetMatch);
+  const deployDirPath = getFullPath(deployDir)
+  const filesToDeploy = getAllFiles(deployDirPath)
+  let filesToGzip = []
 
-  const deployDirPath = getFullPath(deployDir);
-  const filesToDeploy = getAllFiles(deployDirPath);
-  let filesToGzip = [];
+  const bucketDeployPath = parseDeployPath(options.deployPath)
+  const uploadTotal = filesToDeploy.length
+  let uploadCount = 0
+
+  const remotePath = options.staticHosting
+    ? `https://s3-${options.region}.amazonaws.com/${options.bucket}/`
+    : `https://${options.bucket}.s3-website-${options.region}.amazonaws.com/`
+
+  await prepareDeploymentDirectory(options.assetPath, options.assetMatch)
 
   if (options.gzip) {
     filesToGzip = getAllFiles(deployDirPath, options.gzipFilePattern)
-      .map(file => file.absolute);
+      .map(file => file.absolute)
 
-    const filesQueue = [...filesToGzip];
-
-    const gzipPool = new PromisePool(() => {
-      if (filesQueue.length === 0) return null
-
-      const filePath = filesQueue.pop();
-      const outputPath = `${filePath}.gz`;
-      const gzip = zlib.createGzip();
-
-      return new Promise((resolve, reject) => {
-        const input = fs.createReadStream(filePath);
-        const output = fs.createWriteStream(outputPath);
-
-        input.pipe(gzip).pipe(output);
-
-        input.on('error', function(err){
-          reject(err);
-        });
-
-        output.on('error', function(err){
-          reject(err);
-        });
-
-        output.on('finish', function(){
-          resolve();
-          info('✔  ' + filePath);
-        });
-      }).then(() => fs.rename(outputPath, filePath));
-    }, 10);
-
-    try {
-      await gzipPool.start();
-      info('All files gzipped properly.');
-    } catch (err) {
-      error('Unexpected error');
-      error(err);
-    }
+    await gzipFiles(filesToGzip)
   }
-
-  let deployPath = options.deployPath
-  // We don't need a leading slash for root deploys on S3.
-  if (deployPath.startsWith('/')) deployPath = deployPath.slice(1, deployPath.length)
-  // But we do need to make sure there's a trailing one on the path.
-  if (!deployPath.endsWith('/') && deployPath.length > 0) deployPath = deployPath + '/'
-
-  let uploadCount = 0
-  let uploadTotal = filesToDeploy.length
-
-  let remotePath = `https://${options.bucket}.s3-website-${options.region}.amazonaws.com/`
-  if (options.staticHosting) {
-    remotePath = `https://s3-${options.region}.amazonaws.com/${options.bucket}/`
-  }
-
-  info(`Deploying ${filesToDeploy.length} assets from ${deployDirPath} to ${remotePath}`)
 
   const uploadPool = new PromisePool(() => {
     if (filesToDeploy.length === 0) return null
@@ -311,7 +327,7 @@ module.exports = async (options, api) => {
     let filename = filesToDeploy.pop().absolute
     let fileStream = fs.readFileSync(filename)
     let fileKey = filename.replace(deployDirPath, '').replace(/\\/g, '/')
-    let fullFileKey = `${deployPath}${fileKey}`
+    let fullFileKey = `${bucketDeployPath}${fileKey}`
 
     return uploadFile({
       fileKey: fullFileKey,
@@ -336,14 +352,15 @@ module.exports = async (options, api) => {
   }, parseInt(options.uploadConcurrency, 10))
 
   try {
+    spinner.start(`Deploying ${filesToDeploy.length} assets from ${deployDirPath} to ${remotePath}`)
     await uploadPool.start()
-    info('Deployment complete.')
+    spinner.succeed(`All ${filesToDeploy.length} assets have been successfully deployed to ${remotePath}`)
 
     if (options.enableCloudfront) {
       invalidateDistribution(options)
     }
   } catch (uploadErr) {
-    error(`Deployment completed with errors.`)
+    spinner.fail('Deployment completed with errors.');
     error(`${uploadErr.toString()}`)
   }
 }
