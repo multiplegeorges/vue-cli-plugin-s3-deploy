32856668599792ec89f1505862200239c3a1c278
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-05-03 16:39:50 +1200

    Fix default validations

--- src/generator.js
+++ src/generator.js
@@ -1,9 +1,13 @@
+import { VERSION } from './configuration'
+
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     scripts: {
       deploy: 'vue-cli-service s3-deploy'
     }
   })
+
+  options.pluginVersion = VERSION
 
   api.extendPackage({
     vue: {
